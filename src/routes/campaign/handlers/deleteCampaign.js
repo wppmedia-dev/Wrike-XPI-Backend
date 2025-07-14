7c5b0f8d495096ede9198f85baa111e1c8e899d5
Thiyagaraj <ThiyagarajRKO@gmail.com>
2025-07-14 12:56:12 +0530

    Updated delete campaign response

--- src/routes/campaign/handlers/deleteCampaign.js
+++ src/routes/campaign/handlers/deleteCampaign.js
@@ -1,6 +1,8 @@
-import { deleteFolder } from "../../../utils/wrike";
-
-let datahubCustomFieldsData = {};
+import {
+  deleteFolder,
+  getDatahubFields,
+  getDatahubRecords,
+} from "../../../utils/wrike";
 
 export const DeleteCampaign = (wrikeToken, params, fastify) => {
   return new Promise(async (resolve, reject) => {
@@ -22,6 +24,8 @@ export const DeleteCampaign = (wrikeToken, params, fastify) => {
             "Missing parameter! Required parameter is missing for the requested operation.",
         });
 
+      const datahubCustomFieldsData = await getCustomFieldsDatahub(wrikeToken);
+
       // Get folder data
       const wrikeFolderData = await deleteFolder(wrikeToken, folderId);
 
@@ -30,61 +34,62 @@ export const DeleteCampaign = (wrikeToken, params, fastify) => {
         return reject({ message: wrikeFolderData?.errorDescription });
       }
 
+      // // Sending final response
+      // resolve({
+      //   message: "Campaign deleted successfully.",
+      //   data: {
+      //     type: "Campaign",
+      //   },
+      // });
+
+      const folderCustomFieldValues = {};
+
+      for (const [key, value] of Object.entries(datahubCustomFieldsData)) {
+        const cfValue =
+          wrikeFolderData?.data[0]?.customFields?.find(
+            (field) => field.id === value.cfId
+          )?.value ?? "";
+
+        folderCustomFieldValues[key] = cfValue;
+      }
+
       // Sending final response
       resolve({
         message: "Campaign deleted successfully.",
         data: {
           type: "Campaign",
+          // customfieldlist: wrikeFolderData?.data[0]?.customFields,
+          noofcrs: folderCustomFieldValues["noofcrs"],
+          agency: folderCustomFieldValues["agency"],
+          mediabuyingtype: folderCustomFieldValues["mediabuyingtype"],
+          brand: folderCustomFieldValues["brand"],
+          briefeddate: folderCustomFieldValues["briefeddate"],
+          campaignbudget: folderCustomFieldValues["campaignbudget"],
+          campaignenddate: folderCustomFieldValues["campaignenddate"],
+          campaignid: folderCustomFieldValues["campaignid"],
+          campaignname: folderCustomFieldValues["campaignname"],
+          campaignobjective: folderCustomFieldValues["campaignobjective"],
+          campaignstartdate: folderCustomFieldValues["campaignstartdate"],
+          campaignfeedbackstatus:
+            folderCustomFieldValues["campaignfeedbackstatus"],
+          ccuid: folderCustomFieldValues["ccuid"],
+          mediachannelpractice: folderCustomFieldValues["mediachannelpractice"],
+          client: folderCustomFieldValues["client"],
+          comments: folderCustomFieldValues["comments"],
+          cssid: folderCustomFieldValues["cssid"],
+          currency: folderCustomFieldValues["currency"],
+          customerponumber: folderCustomFieldValues["customerponumber"],
+          debtor: folderCustomFieldValues["debtor"],
+          kpiobjective: folderCustomFieldValues["kpiobjective"],
+          originalagency: folderCustomFieldValues["originalagency"],
+          readyforarchive: folderCustomFieldValues["readyforarchive"],
+          region: folderCustomFieldValues["region"],
+          requestedstartdate: folderCustomFieldValues["requestedstartdate"],
+          requestormarket: folderCustomFieldValues["requestormarket"],
+          spacename: folderCustomFieldValues["spacename"],
+          workitemlevel: folderCustomFieldValues["workitemlevel"],
         },
       });
-
-      // const folderCustomFieldValues = {};
-
-      // for (const [key, value] of Object.entries(datahubCustomFieldsData)) {
-      //   const cfValue =
-      //     wrikeFolderData?.data[0]?.customFields?.find(
-      //       (field) => field.id === value.cfId
-      //     )?.value ?? "";
-
-      //   folderCustomFieldValues[key] = cfValue;
-      // }
-
-      // // Sending final response
-      // resolve({
-      //   data: {
-      //     type: "Campaign",
-      //     // customfieldlist: wrikeFolderData?.data[0]?.customFields,
-      //     noofcrs: folderCustomFieldValues["noofcrs"],
-      //     agency: folderCustomFieldValues["agency"],
-      //     mediabuyingtype: folderCustomFieldValues["mediabuyingtype"],
-      //     brand: folderCustomFieldValues["brand"],
-      //     briefeddate: folderCustomFieldValues["briefeddate"],
-      //     campaignbudget: folderCustomFieldValues["campaignbudget"],
-      //     campaignenddate: folderCustomFieldValues["campaignenddate"],
-      //     campaignid: folderCustomFieldValues["campaignid"],
-      //     campaignname: folderCustomFieldValues["campaignname"],
-      //     campaignobjective: folderCustomFieldValues["campaignobjective"],
-      //     campaignstartdate: folderCustomFieldValues["campaignstartdate"],
-      //     campaignfeedbackstatus:
-      //       folderCustomFieldValues["campaignfeedbackstatus"],
-      //     ccuid: folderCustomFieldValues["ccuid"],
-      //     mediachannelpractice: folderCustomFieldValues["mediachannelpractice"],
-      //     client: folderCustomFieldValues["client"],
-      //     comments: folderCustomFieldValues["comments"],
-      //     cssid: folderCustomFieldValues["cssid"],
-      //     currency: folderCustomFieldValues["currency"],
-      //     customerponumber: folderCustomFieldValues["customerponumber"],
-      //     debtor: folderCustomFieldValues["debtor"],
-      //     kpiobjective: folderCustomFieldValues["kpiobjective"],
-      //     originalagency: folderCustomFieldValues["originalagency"],
-      //     readyforarchive: folderCustomFieldValues["readyforarchive"],
-      //     region: folderCustomFieldValues["region"],
-      //     requestedstartdate: folderCustomFieldValues["requestedstartdate"],
-      //     requestormarket: folderCustomFieldValues["requestormarket"],
-      //     spacename: folderCustomFieldValues["spacename"],
-      //     workitemlevel: folderCustomFieldValues["workitemlevel"],
-      //   },
-      // });
     } catch (err) {
       console.log(err?.message || err);
       reject({
@@ -93,4 +98,49 @@ export const DeleteCampaign = (wrikeToken, params, fastify) => {
       });
     }
   });
+};
+
+const getCustomFieldsDatahub = async (wrikeToken) => {
+  try {
+    const datahubFields = await getDatahubFields(
+      wrikeToken,
+      process.env.DATAHUB_CUSTOM_FIELDS_ID
+    );
+
+    if (datahubFields?.errorDescription) {
+      return Promise.reject({
+        errorDescription: datahubFields?.errorDescription,
+      });
+    }
+
+    let formFieldsIds = {};
+    datahubFields?.data?.forEach((field) => {
+      formFieldsIds[field.title?.trim()?.toLowerCase()] = field.id;
+    });
+
+    const datahubRecords = await getDatahubRecords(
+      wrikeToken,
+      process.env.DATAHUB_CUSTOM_FIELDS_ID
+    );
+
+    if (datahubRecords?.errorDescription) {
+      return Promise.reject({
+        errorDescription: datahubRecords?.errorDescription,
+      });
+    }
+
+    let datahubCustomFieldsData = {};
+    datahubRecords?.data?.forEach((record) => {
+      if (record.fieldValues[formFieldsIds["short code"]]?.trim())
+        datahubCustomFieldsData[
+          record.fieldValues[formFieldsIds["short code"]]?.trim()?.toLowerCase()
+        ] = {
+          id: record.id,
+          ["cfId"]: record.fieldValues[formFieldsIds["cf id"]],
+        };
+    });
+    return Promise.resolve(datahubCustomFieldsData);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
