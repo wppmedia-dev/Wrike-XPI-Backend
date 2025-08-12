0df715e32389b33b89b4f12daad9b152e20c8d5a
Thiyagaraj <ThiyagarajRKO@gmail.com>
2025-08-12 16:30:29 +0530

    Added accountId feature

--- src/index.js
+++ src/index.js
@@ -64,6 +64,8 @@ fastify.addHook("onSend", function (request, reply, payload, done) {
 fastify.get("/", async (req, res) => {
   const { WRIKE_LOGIN_ENDPOINT, WRIKE_REDIRECT_URL } = process.env;
 
+  const { accountId } = req.query;
+
   if (!WRIKE_LOGIN_ENDPOINT) {
     throw new Error("Missing WRIKE_LOGIN_ENDPOINT! Please contact your admin");
   }
@@ -78,7 +80,7 @@ fastify.get("/", async (req, res) => {
     });
   }
 
-  const redirectUrl = `${WRIKE_LOGIN_ENDPOINT}/authorize/v4?client_id=${WRIKE_CLIENT_ID}&response_type=code&redirect_uri=${WRIKE_REDIRECT_URL}`;
+  const redirectUrl = `${WRIKE_LOGIN_ENDPOINT}/authorize/v4?accountId=${accountId}&client_id=${WRIKE_CLIENT_ID}&response_type=code&redirect_uri=${WRIKE_REDIRECT_URL}`;
 
   const html = `
 <!DOCTYPE html>
