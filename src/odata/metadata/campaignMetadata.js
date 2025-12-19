1909c18da55f8068da35db21455e239244277cd4
Thiyagaraj Adaikalam <thiyagaraj.adaikalam@groupm.com>
2025-12-19 10:46:52 +0530

    Refactor campaign metadata to use specific EntityType and update EntitySet reference

--- src/odata/metadata/campaignMetadata.js
+++ src/odata/metadata/campaignMetadata.js
@@ -6,13 +6,12 @@ module.exports = `<?xml version="1.0" encoding="utf-8"?>
    xmlns="http://docs.oasis-open.org/odata/ns/edm">
 
     <!-- Allow dynamic properties -->
-    <EntityType Name="UntypedEntity" OpenType="true">
+    <EntityType Name="Campaign">
       <Key><PropertyRef Name="id"/></Key>
-      <Property Name="id" Type="Edm.String"/>
     </EntityType>
 
     <EntityContainer Name="Container">
-      <EntitySet Name="Campaigns" EntityType="UntypedNS.UntypedEntity"/>
+      <EntitySet Name="Campaigns" EntityType="UntypedNS.Campaign"/>
     </EntityContainer>
 
   </Schema>
